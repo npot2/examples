eee9e2455a4770e5c283249477cdd31da1948545
Steve <101606924+npot2@users.noreply.github.com>
2022-12-28 23:51:33 -0800

    Create subscriber.c

---
+++ nng/pubsub/subscriber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <nng/nng.h>
+
+int main() {
+  nng_socket subscriber;
+  nng_msg *msg;
+  char *data;
+
+  // Initialize the subscriber socket
+  nng_sub0_open(&subscriber);
+
+  // Connect the subscriber to the publisher
+  nng_dial(subscriber, "tcp://localhost:5000", NULL, 0);
+
+  // Receive the message on the subscriber
+  nng_recvmsg(subscriber, &msg, 0);
+  data = nng_msg_body(msg);
+  printf("Received message: %s\n", data);
+
+  // Clean up
+  nng_close(subscriber);
+  nng_msg_free(msg);
+
+  return 0;
+}
